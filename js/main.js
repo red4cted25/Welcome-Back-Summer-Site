17a2c975c4a032aa1e1f2901fb3a08321d0d688d
Nico <ndiaz413@west-mec.org>
2024-08-09 00:19:44 -0700

    Finished Home page
    Completed and designed the home page. Also fixed some minor issues with fonts and other sections

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 150) { 
-        header.style.backgroundColor = 'rgba(0, 0, 0, 0.8)'; 
-    } else {
-        header.style.backgroundColor = 'transparent';
-    }
+$(document).ready(function(){
+    $(window).on('scroll', function() {
+        var header = $('header');
+        if ($(window).scrollTop() > 150) {
+            header.css('background-color', 'rgba(0, 0, 0, 0.8)');
+        } else {
+            header.css('background-color', 'transparent');
+        }
+    });
+
+    // Animate the first palm tree (from the left)
+    $('#palm1').css('opacity', '1').animate({left: '+=13vw'}, {duration: 3000, step: function(now, fx) {$(this).css('transform', 'rotate(' + (now * 0.15) + 'deg)');}});
+
+    // Animate the second palm tree (from the right)
+    $('#palm2').css('opacity', '1').animate({right: '+=13vw'}, {duration: 3000, step: function(now, fx) {$(this).css('transform', 'rotate(' + (-now * 0.15) + 'deg)');}});
 });
