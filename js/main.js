256477a0e89a717c276bfd249b48f9a6865b27a6
Nico <ndiaz413@west-mec.org>
2024-08-10 01:58:46 -0700

    Updated Gallery and Formatting
    Updated the gallery section with working formatting and completed the activities cards

--- js/main.js
+++ js/main.js
@@ -14,4 +14,33 @@ $(document).ready(function(){
     // Animate the second palm tree (from the right)
     $('#palm2').css('opacity', '1').animate({right: '+=13vw'}, {duration: 3000, step: function(now, fx) {$(this).css('transform', 'rotate(' + (-now * 0.15) + 'deg)');}});
 
+    $('#gallery .column img').on('mouseenter', function() {
+        var randomRotation = Math.floor(Math.random() * 21) - 10; // Random number between -15 and 15
+        $(this).css('transform', 'scale(1.05) rotate(' + randomRotation + 'deg)');
+    }).on('mouseleave', function() {
+        $(this).css('transform', 'scale(1)');
+    });
 });
+
+// Image Carousel
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("coolSlide");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slides[slideIndex-1].style.display = "block"; 
+}
